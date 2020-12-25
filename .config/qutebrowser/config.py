9a2c56abfcc849a8bf135e54a9c6a86d420138cc
zhuhaiyang <zhuhaiyang.hfuter@outlook.com>
2020-12-25 20:37:51 +0800

    update qutebrowser 👌

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,11 +4,14 @@ c.colors.webpage.prefers_color_scheme_dark = True
 c.completion.shrink = True
 c.completion.use_best_match = True
 c.statusbar.widgets = ["progress", "keypress", "url", "history"]
-c.scrolling.bar = "always"
+c.scrolling.bar = "never"
 c.tabs.position = "top"
 c.tabs.width = "15%"
 c.tabs.title.format = "{index}: {audio}{current_title}"
 c.tabs.title.format_pinned = "{index}: {audio}{current_title}"
+c.statusbar.show = "never"
+c.colors.webpage.darkmode.enabled = True
+
 
 # general
 c.auto_save.session = True
@@ -16,22 +19,25 @@ c.content.default_encoding = "utf-8"
 c.content.javascript.can_access_clipboard = True
 c.content.notifications = True  # notifications aren't supported now anyway
 c.content.pdfjs = True
-c.editor.command = ["kitty", "kak", "-e", "exec {line}g{column0}l", "{file}"]
+c.editor.command = ["kitty", "nvim", "-e", "exec {line}g{column0}l", "{file}"]
 c.downloads.location.prompt = False
 c.input.insert_mode.auto_load = True
 #c.spellcheck.languages = ["en-US"]
-c.content.proxy= "socks://localhost:7891/"
+c.content.proxy = "socks://localhost:7891/"
 c.tabs.background = True
 c.tabs.last_close = "close"
 c.tabs.mousewheel_switching = False
-c.qt.args += ["enable-gpu-rasterization", "enable-features=WebRTCPipeWireCapturer"]
+c.qt.args += ["enable-gpu-rasterization",
+              "enable-features=WebRTCPipeWireCapturer"]
 
 # per-domain settings
-config.set("content.register_protocol_handler", True, "*://calendar.google.com")
+config.set("content.register_protocol_handler",
+           True, "*://calendar.google.com")
 
 config.set("content.desktop_capture", True, "*://app.wire.com")
 
-config.set("content.register_protocol_handler", True, "*://teams.microsoft.com")
+config.set("content.register_protocol_handler",
+           True, "*://teams.microsoft.com")
 config.set("content.desktop_capture", True, "*://teams.microsoft.com")
 
 config.set("content.register_protocol_handler", True, "*://app.slack.com")
@@ -43,29 +49,36 @@ c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 c.content.site_specific_quirks = False
 c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
 
+c.downloads.location.directory = '/home/zhuhaiyang/Downloads'
 # urls
 c.url.searchengines = {
     "DEFAULT": "https://google.com/search?q={}",
     "?": "https://google.com/search?q={}",
 }
 
-c.url.default_page = "https://start.duckduckgo.com/"
-c.url.start_pages = ["https://start.duckduckgo.com/"]
-#c.url.default_page = "~/.config/qutebrowser/blank.html"
-#c.url.start_pages = ["~/.config/qutebrowser/blank.html"]
+c.url.default_page = "~/.config/qutebrowser/startpage/index.html"
+c.url.start_pages = ["~/.config/qutebrowser/startpage/index.html"]
 
 # keys
 bindings = {
-    ",m": "spawn --userscript view_in_mpv",
-    ",M": "hint links spawn mpv {hint-url}",
-    ",p": "spawn --userscript qute-pass --username-target secret --username-pattern 'user: (.+)' --dmenu-invocation 'dmenu -p credentials'",
-    ",P": "spawn --userscript qute-pass --username-target secret --username-pattern 'user: (.+)' --dmenu-invocation 'dmenu -p password' --password-only",
-    ",b": "config-cycle colors.webpage.bg '#32302f' 'white'",
-    "<Ctrl-Shift-J>": "tab-move +",
-    "<Ctrl-Shift-K>": "tab-move -",
-    "M": "nop",
+    ',t': ':open https://twitter.com',
+    ',y': ':open https://www.youtube.com/',
+    ',l': ':open https://wangchujiang.com/linux-command/',
+    ',g': ':open https://github.com/',
+    ',a': ':open https://wiki.archlinux.org/',
+    ',r': ':open https://www.reddit.com/',
+    ',h': ':open https://news.ycombinator.com/news',
+    ',d': ':open https://www.deepl.com/translator',
+    ',m': 'hint links spawn kitty mpv {hint-url}',
+    ',Z': 'hint links spawn kitty -e youtube-dl {hint-url}',
+    't':'set-cmd-text -s :open -t',
+    'xb': 'config-cycle statusbar.show always never',
+    'xt': 'config-cycle tabs.show always never',
+    'xx': 'config-cycle statusbar.show always never;; config-cycle tabs.show always never',
+
     "co": "nop",
 }
 
 for key, bind in bindings.items():
     config.bind(key, bind)
+
